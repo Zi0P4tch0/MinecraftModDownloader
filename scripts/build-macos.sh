c574fddf195f529c185e3241ec6a167fe37343cb
Matteo Pacini <ispeakprogramming@gmail.com>
2016-12-21 00:02:54 +0100

    Fixed macOS build script

--- scripts/build-macos.sh
+++ scripts/build-macos.sh
@@ -8,14 +8,17 @@ TMP_DIR=$(mktemp -d)
 function cleanup {
   echo "Removing temporary directory: $TMP_DIR."
   rm -rf $TMP_DIR
+
 }
 
 trap cleanup EXIT
 
 git reset --hard && git clean -xdf
 pyinstaller --clean --onefile mmd
-cp -r darwin dist
-zip -j -r $ZIP dist/mmd dist/darwin
+cd dist
+cp -r ../darwin .
+zip -r $ZIP mmd darwin/
 cp $ZIP $TMP_DIR
+cd ..
 git reset --hard && git clean -xdf
 mv $TMP_DIR/$ZIP .
