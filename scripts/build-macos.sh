fda9923d61906f18678410fd550b9a2c8ca83458
Matteo Pacini <ispeakprogramming@gmail.com>
2016-12-20 23:55:50 +0100

    Improved macOS build script

--- scripts/build-macos.sh
+++ scripts/build-macos.sh
@@ -5,13 +5,17 @@ MACOS_VERSION=$(sw_vers -productVersion)
 ZIP="mmd-$LATEST_TAG-macOS$MACOS_VERSION+.zip"
 TMP_DIR=$(mktemp -d)
 
+function cleanup {
+  echo "Removing temporary directory: $TMP_DIR."
+  rm -rf $TMP_DIR
+}
+
+trap cleanup EXIT
+
 git reset --hard && git clean -xdf
 pyinstaller --clean --onefile mmd
-cd dist
-cp -r ../darwin .
-zip -r $ZIP mmd darwin/
+cp -r darwin dist
+zip -j -r $ZIP dist/mmd dist/darwin
 cp $ZIP $TMP_DIR
-cd ..
 git reset --hard && git clean -xdf
 mv $TMP_DIR/$ZIP .
-rm -rf $TMP_DIR
